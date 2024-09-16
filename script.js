615e905b5799b97c0887a75e6f9760e45ac537f4
coreyde <corey_239@hotmail.com>
2024-09-16 17:13:22 +1000

    added feature to update display based on btn press

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let operand1 = 0;
 let operand2 = 0;
-let operator = "";
+let curValue = 0;
 
-function operate(operand1, operator, operand2) {
+const display = document.querySelector(".display");
+
+function performOperation(operand1, operator, operand2) {
   switch (operator) {
     case "+":
       add(operand1, operand2);
@@ -28,5 +30,47 @@ function multiply(operand1, operand2) {
 }
 
 function divide(operand1, operand2) {
-  return operand1 / operand2;
+  if (operand2 === 0) {
+    alert("Cannot divide a number by 0!");
+  } else {
+    return operand1 / operand2;
+  }
 }
+
+function buttonPressed() {
+  let buttons = document.querySelectorAll(".btn");
+  let number = "";
+  let operator = "";
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      let keyPress = button.dataset.value;
+
+      if (isNaN(keyPress)) {
+        updateDisplay(number);
+        operator += keyPress;
+        number = "";
+      } else {
+        number += keyPress;
+        console.log(number);
+        updateDisplay(number);
+      }
+    });
+  });
+}
+
+function updateDisplay(number) {
+  if (parseInt(number)) {
+    display.textContent = number;
+  } else {
+    console.log(number);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  buttonPressed();
+});
+
+//store var1 and var2 so that you can do performOperation()
+//do performOperation() function in buttonPressed()
+//update display with final result
